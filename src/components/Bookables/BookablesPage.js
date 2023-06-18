a866c3e8ef8d00bd44070e1d515360b9a195280d
SOUVIGHOSH <souvik.personal.email@gmail.com>
2023-06-18 23:19:09 +0530

    Completed useState

--- src/components/Bookables/BookablesPage.js
+++ src/components/Bookables/BookablesPage.js
@@ -10,6 +10,16 @@ export default function BookablesPage() {
   const [filteredBookables, setFilteredBookables] = useState(
     bookables.filter((b) => b.group === group)
   );
+  const [showDetails, setShowDetails] = useState(false);
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   const onNext = () => {
     setBookableIndex((prev) => (prev + 1) % filteredBookables.length);
   };
@@ -20,28 +30,61 @@ export default function BookablesPage() {
     setFilteredBookables(bookables.filter((b) => b.group !== group));
   };
   return (
-    <ul className="bookables">
-      <select
-        defaultValue={group}
-        onChange={(event) => groupChangeHandler(event.target.value)}
-      >
-        {[...new Set(bookables.map((item) => item.group))].map((group) => (
-          <option value={group}>{group}</option>
-        ))}
-      </select>
-      {filteredBookables.map((b, id) => (
-        <li
-          key={id}
-          className={id === bookableIndex ? "selected" : null}
-          onClick={() => setBookableIndex(id)}
+    <div className="bookables-container">
+      <ul className="bookables">
+        <select
+          defaultValue={group}
+          onChange={(event) => groupChangeHandler(event.target.value)}
         >
-          {b.title}
+          {[...new Set(bookables.map((item) => item.group))].map((group) => (
+            <option value={group}>{group}</option>
+          ))}
+        </select>
+        {filteredBookables.map((b, id) => (
+          <li
+            key={id}
+            className={id === bookableIndex ? "selected" : null}
+            onClick={() => setBookableIndex(id)}
+          >
+            {b.title}
+          </li>
+        ))}
+        <li className="bookables-next" onClick={onNext}>
+          <FontAwesomeIcon icon={faArrowRight} />
+          <span>Next</span>
         </li>
-      ))}
-      <li className="bookables-next" onClick={onNext}>
-        <FontAwesomeIcon icon={faArrowRight} />
-        <span>Next</span>
-      </li>
-    </ul>
+      </ul>
+      <div className="bookable-details">
+        <div className="bookable-details__header">
+          {filteredBookables[bookableIndex].title}
+          <div>
+            <input
+              type="checkbox"
+              value={showDetails}
+              name="showDetails"
+              id="showDetails"
+              onChange={() => setShowDetails((prev) => !prev)}
+            />
+            <label for="showDetails">Show details</label>
+          </div>
+        </div>
+        <div className="bookable-details__description">
+          <div>{filteredBookables[bookableIndex].notes}</div>
+          {showDetails && (
+            <div className="bookable-details__details">
+              <h2>Availability</h2>
+              <ul>
+                <p>
+                  {" "}
+                  {filteredBookables[bookableIndex].days.map((day) => (
+                    <li>{weekdays[day]}</li>
+                  ))}{" "}
+                </p>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
